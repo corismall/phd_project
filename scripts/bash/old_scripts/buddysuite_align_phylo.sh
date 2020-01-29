1524a6de0b403947c4a3843eaf70b78adb346eea
c_desktop <corinn.small@gmail.com>
2020-01-29 12:33:27 +0100

    organizing

--- scripts/bash/old_scripts/buddysuite_align_phylo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-HOME=/home/corinn/Documents/sequences/ITS/set1_26_7_19/generated_seqs
-SEQS=$HOME/all_isos.fasta
-
-alignbuddy SEQS -ga "mafft" -p "--auto --thread 8"
--- scripts/bash/old_scripts/rename_files.sh
+++
@@ -1,6 +0,0 @@
-file=aln_test.sam	
-mv "$file" "${file//aln/As}" 
-
-for file in aln_*.sam ; do 
-	mv "$file" "${file//aln/As}" 
-done
